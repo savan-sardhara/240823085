033563afc1c64133e886efa707da03871812437b
savan-sardhara <savansardhara36@gmail.com>
2025-07-14 10:55:11 +0530

    first two program

---
+++ index.js
@@ -0,0 +1,16 @@
+
+// even odd program
+// const num = 10
+// if(num%2==0){
+//     console.log("Even ")
+// }else{
+//     console.log("odd")
+// }
+
+
+// max of two numbers 
+
+const a=20
+const b=70
+const res = a>b ? a : b
+console.log(`max is ${res}`)
